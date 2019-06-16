327ac9d4c6f01255402418e0e37c9dc6a40cab29
ibrahim samak <bobsamak@gmail.com>
2019-06-16 15:46:26 +0300

    semi final version 26

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -185,17 +185,39 @@ exports.getDailyRevenu = async (req, reply) => {
 
         console.log('today1' + new Date(new Date().setHours(00, 00, 00)))
         console.log('today2' + new Date(new Date().setHours(23, 59, 59)))
-        const newOrders = await Order.find({ createAt: { $gte: cc, $lt: cc }, StatusId: 1 }).count()
+
+        const newOrders = await Order.find({
+            $and: [{
+                createAt: {
+                    $gte: new Date(new Date().setHours(00, 00, 00)),
+                    $lt: new Date(new Date().setHours(24, 00, 00))
+                }
+            }, { StatusId: 1 }]
+        }).count()
         const _all = await Order.find({
             $and: [{
                 createAt: {
                     $gte: new Date(new Date().setHours(00, 00, 00)),
                     $lt: new Date(new Date().setHours(24, 00, 00))
                 }
-            },{$or: [{ StatusId: 3 }, { StatusId: 4 }]}]
+            }, { $or: [{ StatusId: 3 }, { StatusId: 4 }] }]
         }).count()
-        const cancelOrder_drivers = await Order.find({ createAt: { $gte: cc, $lt: cc }, StatusId: 6 }).count()
-        const cancelOrder_users = await Order.find({ createAt: { $gte: cc, $lt: cc }, StatusId: 5 }).count()
+        const cancelOrder_drivers = await Order.find({
+            $and: [{
+                createAt: {
+                    $gte: new Date(new Date().setHours(00, 00, 00)),
+                    $lt: new Date(new Date().setHours(24, 00, 00))
+                }
+            }, { StatusId: 6 }]
+        }).count()
+        const cancelOrder_users = await Order.find({
+            $and: [{
+                createAt: {
+                    $gte: new Date(new Date().setHours(00, 00, 00)),
+                    $lt: new Date(new Date().setHours(24, 00, 00))
+                }
+            }, { StatusId: 5 }]
+        }).count()
 
         _items.push(
             { _all: _all },
